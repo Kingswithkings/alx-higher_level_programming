79a9cfd46d1e3d9ba225a0f009a426fa3ae3623d
kingswithkings <kingsuthanaidogu@gmail.com>
2023-05-16 21:47:01 -0700

    1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,5 @@
--- a script that deletes the database hbtn_0c_0 in your MySQL server.
--- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+-- a script that creates the database hbtn_0c_0 in my MySQL server.
+-- If the database hbtn_0c_0 already exists, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements
-DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
